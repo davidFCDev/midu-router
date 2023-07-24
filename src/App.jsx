a56655ef05de0bd1319e22fd4fb597489ad95560
David Fernández <davidfernandezcomesana@gmail.com>
2023-07-24 13:11:45 +0200

    creamos evento navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 
+const NAVIGATION_EVENT = "pushstate";
+
+function navigate(href) {
+  window.history.pushState({}, "", href);
+  // crear un evento personalizado
+  const navigationEvent = new Event(NAVIGATION_EVENT);
+  window.dispatchEvent(navigationEvent);
+}
+
 function HomePage() {
   return (
     <>
