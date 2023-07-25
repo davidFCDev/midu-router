065cfb45b1e12def25cf4fac21bb678a5b157dbf
David Fernández <davidfernandezcomesana@gmail.com>
2023-07-25 18:42:07 +0200

    añadimos routes

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,8 +1,12 @@
-import { useEffect, useState } from "react";
+import { Children, useEffect, useState } from "react";
 import { EVENTS } from "./consts";
 import { match } from "path-to-regexp";
 
-export function Router({ routes = [], DefaultComponent = () => <h1>404</h1> }) {
+export function Router({
+  children,
+  routes = [],
+  DefaultComponent = () => <h1>404</h1>,
+}) {
   const [currentPath, setCurrentPath] = useState(window.location.pathname);
 
   useEffect(() => {
@@ -21,11 +25,20 @@ export function Router({ routes = [], DefaultComponent = () => <h1>404</h1> }) {
 
   let routeParams = {};
 
-  const Page = routes.find(({ path }) => {
+  // add routes from children <Route /> components
+  const routesFromChildren = Children.map(children, ({ props, type }) => {
+    const { name } = type;
+    const isRoute = name === "Route";
+    return isRoute ? props : null;
+  });
+
+  const routesToUse = routes.concat(routesFromChildren);
+
+  const Page = routesToUse.find(({ path }) => {
     if (path === currentPath) return true;
 
     // usamos path-to-regexp para detectar rutas dinamicas
-    
+
     const matcherUrl = match(path, { decode: decodeURIComponent });
     const matched = matcherUrl(currentPath);
     if (!matched) return false;
